060a0acfced628fbacd4a6fafa0b94861902a20d
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-09 20:49:33 +0530

    Changes with custom module to open camera activity from android

--- android/app/src/main/java/com/rn_object_detection/CustomCamera/CustomCamera.kt
+++ android/app/src/main/java/com/rn_object_detection/CustomCamera/CustomCamera.kt
@@ -1,4 +1,32 @@
 package com.rn_object_detection.CustomCamera
+import android.content.Intent
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import android.util.Log
+import androidx.core.content.ContextCompat.startActivity
+import com.rn_object_detection.CameraActivity
 
-class CustomCamera {
+class CustomCamera(reactContext: ReactApplicationContext) :
+ReactContextBaseJavaModule(reactContext){
+
+    private val packageName: String = "CustomCamera"
+    private val tag:String = "TAG"
+    override fun getName(): String {
+        return packageName
+    }
+
+    @ReactMethod
+    fun printMessage(message: String) {
+        Log.d(tag, "Message: $message")
+    }
+
+    @ReactMethod
+    fun openCameraActivity() {
+        val intent = Intent(currentActivity, CameraActivity::class.java)
+        currentActivity?.startActivity(intent)
+    }
+
 }
--- android/app/src/main/java/com/rn_object_detection/RootAppPackage.kt
+++ android/app/src/main/java/com/rn_object_detection/RootAppPackage.kt
@@ -1,4 +1,24 @@
 package com.rn_object_detection
 
-class RootAppPackage {
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+import com.rn_object_detection.CustomCamera.CustomCamera
+
+class RootAppPackage: ReactPackage {
+    override fun createNativeModules(
+        reactContext: ReactApplicationContext
+    ): MutableList<NativeModule> {
+        var modules = arrayListOf<NativeModule>()
+        modules.add(CustomCamera(reactContext))
+        modules.toMutableList()
+        return modules
+    }
+
+    override fun createViewManagers(
+        reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 }
