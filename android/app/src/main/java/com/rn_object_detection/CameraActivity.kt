f9ed96a697aeee6367451166851b290a3db11be6
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-09 21:30:03 +0530

    Added - basic layout for capture camera activity

--- android/app/src/main/java/com/rn_object_detection/CameraActivity.kt
+++ android/app/src/main/java/com/rn_object_detection/CameraActivity.kt
@@ -1,12 +1,32 @@
 package com.rn_object_detection
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Paint
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.os.Handler
+import android.view.TextureView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CameraActivity : AppCompatActivity() {
+    lateinit var textureView: TextureView
+    lateinit var cameraManager: CameraManager
+    lateinit var handler: Handler
+    lateinit var cameraDevice: CameraDevice
+    lateinit var imageView: ImageView
+    lateinit var bitmap: Bitmap
+    lateinit var labels:List<String>
+    val paint = Paint()
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
